ba659c424d65251e418c87ccf31d8bdb32da3000
A7Zulu <A7Zulu@gmail.com>
2013-12-20 19:10:04 +0000

    updated a few lines

--- python-dev.pp
+++ python-dev.pp
@@ -12,4 +12,17 @@ class core {
 	}
 }
 
+class python {
+	package {
+	[ "python", "python-setuptools", "python-dev", "python-pip",
+	  "python-matplotlib", "python-imaging", "python-numpy", "python-scipy",
+	  "python-software-properties", "idle", "python-qt4", "python-wsgtk2.8"
+	]:
+	ensure => ["installed"],
+	require => Exec['apt-update']
+	}
+
+}
+
+
 include core
